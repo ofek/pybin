1d1c8e0a81532cfda88ce5601f2880430b5e8646
ofek <ofekmeister@gmail.com>
2017-08-31 18:46:12 -0400

    do cli

--- pybin/cli.py
+++ pybin/cli.py
@@ -1,12 +1,32 @@
 import click
 
+from pybin import in_path, locate, put_in_path
+
 
 CONTEXT_SETTINGS = {
     'help_option_names': ['-h', '--help'],
 }
 
 
-@click.group(context_settings=CONTEXT_SETTINGS)
+@click.group(invoke_without_command=True, context_settings=CONTEXT_SETTINGS)
+@click.option('-p', '--pypath')
 @click.version_option()
-def pybin():
-    pass
+@click.pass_context
+def pybin(ctx, pypath):
+    if ctx.invoked_subcommand is None:
+        location = locate(pypath)
+        exists = in_path(pypath)
+        click.echo('The user bin directory `{}` is {} in PATH{}'.format(
+            location,
+            'already' if exists else 'not',
+            '!' if exists else '.'
+        ))
+
+
+@pybin.command(context_settings=CONTEXT_SETTINGS)
+@click.option('-p', '--pypath')
+def put(pypath):
+    if in_path(pypath):
+        click.echo('The user bin directory `{}` is already in PATH!'.format(locate(pypath)))
+
+    click.echo('Success!' if put_in_path(pypath) else 'Unexpected failure')
